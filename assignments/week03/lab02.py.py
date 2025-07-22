58b06558dcab961084f81308271c24decfe12c03
Theerin <Theerin.s@ku.th>
2025-07-22 15:18:37 +0700

    t.py

---
+++ assignments/week03/lab02.py.py
@@ -0,0 +1,37 @@
+# Complete this ATM simulation
+balance = 1000
+pin = "1234"
+ 
+entered_pin = input("Enter PIN: ")
+if entered_pin == pin:
+    print("PIN accepted")
+    while True:
+        print("\n1. Check Balance")
+        print("2. Withdraw")
+        print("3. Deposit")
+        print("4. Exit")
+       
+        choice = input("Choose option: ")
+       
+        # Complete the menu logic here
+        # Your code here:
+        if choice == "4"
+        break
+ 
+    if choice == "3":
+        deosit = float input(input("deposit amount: "))
+        if deposit <0:
+         print("Cannot deposit less than 0")
+        contiue
+ 
+    if choice == "2"
+       withdraw = float(input(("withdraw amount:")))
+       balance = balance - withdraw
+       print("successfuly, Your balance now =",balance)
+ 
+    if choice =="1"
+     print("Your balance now="balance)
+else
+    contiue
+else:
+    print("Invalid PIN")
